37c9792494ef01fa53a326a1eb9b182e799ac0bc
Aaron (@Relbeits) <euaaron@outlook.com>
2019-09-18 08:20:14 -0300

    Update main.c

--- dataStructure/struct/main.c
+++ dataStructure/struct/main.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main(void) 
+{
+	typedef struct { 
+    	int id;
+    	int hp;
+    	float cp;    
+    	char nome;
+    	int totalEvo;
+    	int atualEvo;    
+	} Pokemon;
+	
+	Pokemon *pikachu = NULL;
+	
+	if(pikachu!=NULL) {
+		printf("Não funciona!");
+	}
+	
+	
+	return 0;
+}
